4ba0dcbe63640423952baebb5aefc56e9aa06691
timothy-coder <nicopar666xd@gmail.com>
2025-08-01 19:32:15 -0500

    commit

--- blogs/models.py
+++ blogs/models.py
@@ -6,7 +6,7 @@ class Blog(models.Model):
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    isActive = models.BooleanField(default=True)
+    isActive = models.BooleanField(default=False)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
     producto = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True, blank=True)  # 👈 CAMBIO
 
